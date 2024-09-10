e0085d5ebfd032f12237f439800145f96ec2f497
yangzhang31 <yangz15@nycstudents.net>
2024-09-10 16:54:56 -0400

    Yang Zhang Lab 1
    Lab 1

---
+++ Zhang_Yang_Lab_1.cpp
@@ -0,0 +1,43 @@
+// Yang Zhang 
+// Lab 1
+// 9.10.2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() // This is the function named main
+{
+    cout << "####################################################################################" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                        #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#       Press the key combination Ctrl + Shift + Escape                            #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#       Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#       Type “Task Manager” in the Windows Start menu search                       #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "#                                                                                  #" << endl;
+    cout << "####################################################################################" << endl;
+
+    return 0; // This is the return to fuction named main
+
+}
+
+/*
+####################################################################################
+#                                                                                  #
+#                                                                                  #
+# Ways to access the Task Manager on your Windows computer:                        #
+#                                                                                  #
+#       Press the key combination Ctrl + Shift + Escape                            #
+#                                                                                  #
+#       Press the key combination Ctrl + Alt + Delete and select “Task Manager”    #
+#                                                                                  #
+#       Type “Task Manager” in the Windows Start menu search                       #
+#                                                                                  #
+#                                                                                  #
+####################################################################################
+*/
